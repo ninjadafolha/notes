3fbdc79236592e4b126f700355c916da77a602bb
Gabriel Barcellos Kretli Lopes <sd46196@fiatauto.adfa.local>
2023-06-07 20:20:23 -0300

    adding part of the struct notes

--- notes_struct.c
+++ notes_struct.c
@@ -1,2 +1,37 @@
 /*
+    STRUCTURES AND UNIONS
+
+    Structure is a data structure used to create user-defined data types in C
+    It allows to combune data of different types
+
+    struct tag_name{
+        member_element-1;
+        member_element-2;
+        member_element-3;
+        member_element-n;
+    };
+
 */
+// definition does not consume memory
+struct CarModel{
+    unsigned carNumber;
+    uint32_t carPrice;
+    uint16_t carMaxSpeed;
+    float    carWeight;
+};
+
+int main(void){
+    struct CarModel carGabriel = {2021,15000,220,1330};
+    struct CarModel carOther   = {4031, 35000, 160, 1900.96};
+
+    printf("Sizeof of struct carModel is %I64u\n", sizeof(struct carModel));
+
+    struct CarModel carMatheus = {.carWeight=90.9, .carPrice=15000};
+
+    printf("Details of car Gabriel\n");
+    printf("carNumber = %u\n", carGabriel.carNumber);
+    printf("carNumber = %u\n", carGabriel.carPrice);
+    printf("carNumber = %u\n", carGabriel.carMaxSpeed);
+    printf("carNumber = %u\n", carGabriel.carWeight);
+
+}
